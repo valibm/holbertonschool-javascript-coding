c58b2dfc153ab49a0d0e63728e888f8df63027a8
valibm <vmustafayev2001@gmail.com>
2024-05-28 09:56:10 +0000

    Handled

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
+
 module.exports = app;
